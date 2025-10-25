a7e44c4290a241f604aaa5445f9f615b38f798cd
flohms18 <trouilletflorent@outlook.com>
2025-10-25 18:11:01 +0200

    GuessNumber added

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,3 +1,28 @@
 import random as rd
 
+def GuessNumber():
+    Count = 0
+    RandomNumber = rd.randint(1,100)
+    
+    while True :
+        MyNumber = input("Pick a number: ")
+        if MyNumber > RandomNumber :
+            print("Too High!")
+            Count += 1
+            if Count == 3:
+                print("Game Over!")
+                break
+        elif MyNumber < RandomNumber :
+            print("Too low!")
+            Count += 1
+            if Count == 3:
+                print("Game Over!")
+                break
+        else:
+            print("Well done!")
+            break
+
+GuessNumber()
+
+
 
