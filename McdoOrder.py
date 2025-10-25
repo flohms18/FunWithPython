3c8246534594e30894056c81573c85173b85ef06
flohms18 <trouilletflorent@outlook.com>
2025-10-25 18:59:06 +0200

    Add Menu

--- McdoOrder.py
+++ McdoOrder.py
@@ -1,2 +1,14 @@
 def Order():
+    Order = []
+    Menu = []
     print("Welcome to Mcdonald's!")
+    ChoiceMenu = input("Would you like to order a menu? Y/N")
+    if ChoiceMenu == "Y":
+        Burger = input("Which Burger do you want? (BigMac/Cheeseburger)")
+        if Burger == "CheeseBurger":
+            ExtraCheese = input("Add Extra Cheese?")
+            if ExtraCheese == "Y":
+                Menu.append("ExtraCheese")
+            ExtraBacon = input("Add Extra Bacon?")
+
+
